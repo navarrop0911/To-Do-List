de529b6abaeb30e074e3a6c4fc93fa4b5d0b0073
Paola <navarrop0911@gmail.com>
2024-02-21 09:53:02 -0800

    created html, css, and js files

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('My code is running');
+
+let btn = document.querySelector('#AddToDo');
+btn.addEventListener("click", () => alert('Task was added to the To-Do-List'))
+
+let ToDoListItems = []
+
+function addToDoItem(item) {
+  toDoListItems.push(item);
+}
+
